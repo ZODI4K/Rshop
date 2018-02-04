a667082037a6b30fd6b9881c7cc11eac34efe0d0
zodi4k <s3th4130@gmail.com>
2018-02-04 23:32:31 +0100

    Added ViewModel dependency, product showcase view model, and layout.

---
+++ app/src/main/java/com/samsung/rshop/ProductShowcaseViewModel.java
@@ -0,0 +1,12 @@
+package com.samsung.rshop;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by zodi4k on 04.02.18.
+ */
+
+public class ProductShowcaseViewModel extends ViewModel {
+}
+//TODO recycler view of shopping products
+//TODO fill with  REST api content
